5b625228130877f5a7966e0ac4c3f5ede1fab517
Lastrevio <stefanzingzing@yahoo.com>
2026-03-22 18:29:16 +0000

    Finally managed to make airflow work through Docker

---
+++ dags/myanimelist_pipeline.py
@@ -0,0 +1,61 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import papermill as pm
+import shutil
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+}
+
+def run_notebook():
+    pm.execute_notebook(
+        input_path='/workspace/notebooks/load_curated_tables.ipynb',
+        output_path='/workspace/notebooks/load_curated_tables_output.ipynb',
+        kernel_name='python3'
+    )
+
+def copy_duckdb():
+    shutil.copyfile(
+        '/workspace/data/pipeline.duckdb',
+        '/workspace/pipeline.duckdb'
+    )
+
+with DAG(
+    dag_id='myanimelist_pipeline',
+    default_args=default_args,
+    description='Monthly MAL data pipeline',
+    schedule_interval='@monthly',
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+    tags=['myanimelist'],
+) as dag:
+
+    fetch_raw = BashOperator(
+        task_id='fetch_raw_json',
+        bash_command='cd /workspace && python -c "import sys; sys.path.append(\'/workspace/python\'); from fetch_raw_json import fetch_new_importbatch; fetch_new_importbatch()"',
+    )
+
+    load_bronze = BashOperator(
+        task_id='load_bronze',
+        bash_command='cd /workspace && python -c "import sys; sys.path.append(\'/workspace/python\'); from fetch_raw_json import load_missing_batches; load_missing_batches()"',
+    )
+
+    load_curated = PythonOperator(
+        task_id='load_curated_tables',
+        python_callable=run_notebook,
+    )
+
+    run_dbt = BashOperator(
+        task_id='run_dbt',
+        bash_command='cd /workspace/dbt && dbt run',
+    )
+
+    copy_db = PythonOperator(
+        task_id='copy_duckdb',
+        python_callable=copy_duckdb,
+    )
+
+    fetch_raw >> load_bronze >> load_curated >> run_dbt >> copy_db
--- dbt/models/duckdb_queries/testquery.sql
+++ dbt/models/duckdb_queries/testquery.sql
@@ -1,6 +1,6 @@
 with final as (
---SELECT * FROM curated.anime LIMIT 1
---SELECT DISTINCT status FROM curated.anime
-SELECT * FROM datamart.d_licensors
+    SELECT * FROM datamart.f_anime f
+    LEFT JOIN datamart.links_genres l ON f.anime_id = l.anime_id
+    LEFT JOIN datamart.d_genres g ON l.genre_id = g.genre_id
 )
 SELECT * FROM final
